2635a658f6c0310aee52f2e140b8d8734204eea0
jurra <jose.urra86@gmail.com>
2019-08-14 15:38:27 +0200

    ignore images and add link to pages and repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,12 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 
+
 const dirpath = "./docs";
 //list to directories for subtree
 const dirs = fs.readdirSync(dirpath).filter((f) => {
     //ignore directory start with .
-    if (f.charAt(0) === ".") return false;
+    if (f.charAt(0) === "." || f === "images") return false;
 
     return fs.existsSync(dirpath + "/" + f) && fs.statSync(dirpath + "/" + f).isDirectory();
 });
@@ -41,16 +42,45 @@ const sidebarArray = ["/"].concat(dirs.map((dir) => {
 
 // configure these modules if you
 module.exports = {
-    title: "Name your project",
+    title: "Hardocs guide",
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/favicon.ico'
+        }]
+    ],
     //description : "use for meta descriptitrueon",
     base: "/" + process.env.REPOSITORY + "/",
     config: (md) => {
         md.options.linkify = sidebarArray;
     },
     themeConfig: {
-        sidebar: sidebarArray
-    }
-};
+        logo: '/logo.png',
+        nav: [{
+            text: 'Community Chat',
+            link: 'https://t.me/hardocs19'
+        }],
+        sidebar: sidebarArray,
+
+        // Assumes GitHub. Can also be a full GitLab url.
+        repo: 'https://gitlab.com/go-commons/hardocs',
+        // Customising the header label
+        // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+        repoLabel: 'Repository',
+
+        // Optional options for generating "Edit this page" link
+
+        // if your docs are in a different repo from your main project:
+        docsRepo: 'https://github.com/goscommons/hardocs-guide/',
+        // if your docs are not at the root of the repo:
+        docsDir: 'docs',
+        // if your docs are in a specific branch (defaults to 'master'):
+        docsBranch: 'master',
+        // defaults to false, set to true to enable
+        editLinks: true,
+        // custom text for edit link. Defaults to "Edit this page"
+        editLinkText: 'Help us improve this page!'
+    },
+}
 
 //check for sidebar
-console.log(sidebarArray);
